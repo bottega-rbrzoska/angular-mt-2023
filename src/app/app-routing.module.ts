26b53776790f449dc3779a47048bc76f274fc51e
Rafal Brzoska <rafal@brzoska.net.pl>
2023-01-10 11:02:25 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyTestComponent } from './my-test/my-test.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TestComponent },
+  { path: 'test', component: MyTestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
