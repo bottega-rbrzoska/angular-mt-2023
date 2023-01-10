e3d01f9d9e944cd2193f9c4b62bffacc0af9cab5
Rafal Brzoska <rafal@brzoska.net.pl>
2023-01-10 13:15:41 +0100

    products

---
+++ src/app/products/product-add/product-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mt-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.scss']
+})
+export class ProductAddComponent {
+
+}
